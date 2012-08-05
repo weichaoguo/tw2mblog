e123453f213beae78447e802a3b235d51bd3919a
Vector Guo <vectorguo@gmail.com>
2012-08-05 21:28:52 +0800

    re arch publish & collect code ^_^

--- tw2mbot.py
+++ tw2mbot.py
@@ -41,9 +41,6 @@ except ImportError:
 
 import utils
 
-cf = ConfigParser.ConfigParser()
-cf.read("tw2mbot.conf")
-
 def get_tweepy_api(consumer_key, consumer_key_secret, access_token, access_token_secret):
     auth = tweepy.OAuthHandler(consumer_key, consumer_key_secret)
     auth.set_access_token(access_token, access_token_secret)
@@ -62,80 +59,112 @@ def translate_en_zh_cn(key, keyfrom, contents):
     query = {'q':contents}
     return json.loads(urllib.urlopen("http://fanyi.youdao.com/openapi.do?keyfrom="+keyfrom+"&key="+key+"&type=data&doctype=json&version=1.1&"+urllib.urlencode(query)).read())["translation"][0]
 
+def publish(tweet):
+    #print tweet.text
+    content = tweet.text
+    #clean & collect links, tags, mentions
+    urls = re.findall("http://\S+", tweet.text)
+    mentions = re.findall("@\S+", tweet.text)
+    tags = re.findall("#[A-Za-z0-9]+", tweet.text)
+    for url in urls:
+        tweet.text = tweet.text.replace(url, "", 1)
+    for i in range(len(urls)):
+        content = content.replace(urls[i], "("+str(i)+")", 1)
+    for i in range(len(mentions)):
+        content = content.replace(mentions[i], "@"+str(i), 1)
+    for i in range(len(tags)):
+        content = content.replace(tags[i], "["+str(i)+"]", 1)
+    #translation
+    trans = translate_en_zh_cn(youdao_key, youdao_keyfrom, content.encode("utf-8", "ignore"))
+    #trim 
+    trans = trans.strip(' \n\t\r')
+    #replace back mentions & tags etc.
+    for i in range(len(mentions)):
+        trans = trans.replace("@"+str(i), mentions[i]+" ", 1)
+    for i in range(len(tags)):
+        trans = trans.replace("["+str(i)+"]", tags[i]+"#", 1)
+    
+    #translate 'RT ' to Chinese
+    trans = trans.replace('RT ', rt_chinese.decode("utf-8"), 1)
+    
+    #recover t.co short urls then shorten with t.cn
+    for i in range(len(urls)):
+        trans = trans.replace("("+str(i)+")", sinaweibopy_api.get.short_url__shorten(url_long=utils.unshortlink(urls[i], 2))[0]["url_short"], 1)
+    
+    #add prefix ...
+    trans = "#"+followings[tweet.author.screen_name.lower()].decode("utf-8")+"#:" + trans
+    tweet.text = "#"+tweet.author.screen_name+"#:" + tweet.text
+    #print trans
+    #updates tsina & tqq
+    ret = pyqqweibo_api.tweet.add(trans, clientip='127.0.0.1')
+    pyqqweibo_api.tweet.show(ret.id).retweet(tweet.text)
+    sinaweibopy_api.post.statuses__update(status=tweet.text)
+    sinaweibopy_api.post.statuses__update(status=trans)
+    #sinaweibopy_api.post.statuses__repost(id=string.atoi(ret["id"]), status=trans)
+
 def main():
+    cur_order = 0
+    last_order = queue_size - 1
     # since id from twitter
     since_id = cf.get("general", "since_id") 
-    # read conf & get apis
-    rt_chinese = cf.get("general", "rt_chinese")
-    #read youdao fanyi api key & keyfrom
-    youdao_key = cf.get("youdao", "key")
-    youdao_keyfrom = cf.get("youdao", "keyfrom")
-    #read config tweet one time
-    tweets_ot = string.atoi(cf.get("general", "tweets_ot"))
-    #read refresh time in seconds
-    refresh_time = string.atof(cf.get("general", "refresh_time"))
-
-    tweepy_api = get_tweepy_api(cf.get("twitter", "consumer_key"), cf.get("twitter", "consumer_key_secret"), cf.get("twitter", "access_token"), cf.get("twitter", "access_token_secret")) 
-    pyqqweibo_api = get_pyqqweibo_api(cf.get("tqq", "consumer_key"), cf.get("tqq", "consumer_key_secret"), cf.get("tqq", "access_token"), cf.get("tqq", "access_token_secret")) 
-    sinaweibopy_api = get_sinaweibopy_api(cf.get("tsina", "consumer_key"), cf.get("tsina", "consumer_key_secret"), cf.get("tsina", "access_token"), cf.get("tsina", "access_token_secret")) 
-    #refresh every 'refresh_time' in minutes
+    # cache the newest tweet for every following guys
+    tweet_cache = {}.fromkeys(names, None)
+    #is this guys newest tweet published ?
+    published = {}.fromkeys(names, True)
+    #refresh every 'refresh_time' in seconds
     try:
         while True:
-            #collect pop tweets
-            tweets = tweepy_api.home_timeline(since_id=since_id, count=tweets_ot)
-
+            #collect tweets
+            tweets = tweepy_api.home_timeline(since_id=since_id, count=collect_limit)
             for tweet in tweets:
+                #save current max id as new since id
+                since_id = tweet.id if (tweet.id > since_id) else since_id
+                #update tweet cache
+                name = tweet.author.screen_name.lower()
+                if tweet_cache[name] is None or tweet.id > tweet_cache[name].id:
+                    tweet_cache[name] = tweet
+                    published[name] = False
+            update_counter = 0
+            while update_counter < update_limit:
                 try:
-                    #save current max id as new since id
-                    since_id = tweet.id if (tweet.id > since_id) else since_id
-                    #clean & collect links, tags, mentions
-                    urls = re.findall("http://\S+", tweet.text)
-                    mentions = re.findall("@\S+", tweet.text)
-                    tags = re.findall("#\S+", tweet.text)
-                    content = tweet.text
-                    for url in urls:
-                        tweet.text = tweet.text.replace(url, "", 1)
-                    for i in range(len(urls)):
-                        content = content.replace(urls[i], "_"+str(i), 1)
-                    for i in range(len(mentions)):
-                        content = content.replace(mentions[i], "@"+str(i), 1)
-                    for i in range(len(tags)):
-                        content = content.replace(tags[i], "["+str(i), 1)
-
-                    tweet_prefix = "("+tweet.author.screen_name+"):"
-                    content = tweet_prefix + content
-
-                    #translation
-                    trans = translate_en_zh_cn(youdao_key, youdao_keyfrom, content.encode("utf-8", "ignore"))
-                    #trim 
-                    trans = trans.strip(' \n\t\r')
-                    #replace back mentions & tags etc.
-                    for i in range(len(mentions)):
-                        trans = trans.replace("@"+str(i), mentions[i], 1)
-                    for i in range(len(tags)):
-                        trans = trans.replace("["+str(i), tags[i]+"#", 1)
-                    trans = trans.replace("(", "#", 1).replace(")", "#", 1)
-                    tweet_prefix = tweet_prefix.replace("(", "#", 1).replace(")", "#", 1)
-                    
-                    #translate 'RT ' to Chinese
-                    trans = trans.replace('RT ', rt_chinese.decode("utf-8"), 1)
-                    
-                    #recover t.co short urls then shorten with t.cn
-                    for i in range(len(urls)):
-                        trans = trans.replace("_"+str(i), sinaweibopy_api.get.short_url__shorten(url_long=utils.unshortlink(urls[i], 2))[0]["url_short"], 1)
-                    
-                    #print trans
-                    #updates tsina & tqq
-                    ret = pyqqweibo_api.tweet.add(trans, clientip='127.0.0.1')
-                    pyqqweibo_api.tweet.show(ret.id).retweet(tweet_prefix+tweet.text)
-                    ret = sinaweibopy_api.post.statuses__update(status=trans)
-                    #sinaweibopy_api.post.statuses__update(status=tweet_prefix+tweet.text)
-                    #sinaweibopy_api.post.statuses__repost(id=ret['id'], status=tweet_prefix+tweet.text)
+                    if not published[names[cur_order]]:
+                        publish(tweet_cache[names[cur_order]])
+                        published[names[cur_order]] = True
+                        update_counter = update_counter + 1
+                    if cur_order == last_order:
+                        cur_order = (cur_order + 1) % queue_size
+                    cur_order = (cur_order + 1) % queue_size
                 except:
                     continue
+            last_order = (cur_order + queue_size - 1) % queue_size
             time.sleep(refresh_time)
     except KeyboardInterrupt:
         return
+
+#load Consts & APIs & keys
+cf = ConfigParser.ConfigParser()
+cf.read("tw2mbot.conf")
+# read conf & get apis
+rt_chinese = cf.get("general", "rt_chinese")
+#read youdao fanyi api key & keyfrom
+youdao_key = cf.get("youdao", "key")
+youdao_keyfrom = cf.get("youdao", "keyfrom")
+#read limits
+update_limit = string.atoi(cf.get("general", "update_limit"))
+collect_limit = string.atoi(cf.get("general", "collect_limit"))
+#read refresh time in seconds
+refresh_time = string.atof(cf.get("general", "refresh_time"))
+#read followings
+cf.read("followings.list")
+names = cf.options("followings")
+queue_size = len(names)
+followings = {}
+for name in names:
+    followings[name] = cf.get("followings", name)
+
+tweepy_api = get_tweepy_api(cf.get("twitter", "consumer_key"), cf.get("twitter", "consumer_key_secret"), cf.get("twitter", "access_token"), cf.get("twitter", "access_token_secret")) 
+pyqqweibo_api = get_pyqqweibo_api(cf.get("tqq", "consumer_key"), cf.get("tqq", "consumer_key_secret"), cf.get("tqq", "access_token"), cf.get("tqq", "access_token_secret")) 
+sinaweibopy_api = get_sinaweibopy_api(cf.get("tsina", "consumer_key"), cf.get("tsina", "consumer_key_secret"), cf.get("tsina", "access_token"), cf.get("tsina", "access_token_secret")) 
 
 if __name__ == '__main__':
     main()
